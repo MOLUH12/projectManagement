1fa97baa499714642d775fb1b31f2b0402dc7a8c
MOLUH <didaouda22@gmail.com>
2024-06-18 14:53:54 +0200

    Initial commit

---
+++ projectPooA/venv-pooA/todo_project/todo_app/tests.py
@@ -0,0 +1,13 @@
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse # new
+from django.test import SimpleTestCase
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
