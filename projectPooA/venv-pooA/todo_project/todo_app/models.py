83c0f2c17a1d58700cfc94a3ae84a5a62eb94c7a
MOLUH <102315064+MOLUH12@users.noreply.github.com>
2024-06-18 17:01:27 +0200

    Delete projectPooA/venv-pooA directory

--- projectPooA/venv-pooA/todo_project/todo_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return self.title
